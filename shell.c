a6ec48f7459f183aed904a8a1ee5b353b92d2b98
Davis <davisjoseph767@gmail.com>
2023-08-21 12:46:40 -0700

    updated shell.c

--- shell.c
+++ shell.c
@@ -4,27 +4,9 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include "main.h"
 
 #define MAX_INPUT_LENGTH 1024
-
-void execute_command(char *command) {
-    pid_t pid = fork();
-
-    if (pid == -1) {
-        perror("fork");
-    } else if (pid == 0) {
-        /* Child process */
-        char *args[2];
-        args[0] = command;
-        args[1] = NULL;
-        execve(command, args, NULL);
-        perror("./shell");
-        _exit(1);
-    } else {
-        /* Parent process */
-        wait(NULL);
-    }
-}
 
 int main() {
     char *input = NULL;
@@ -55,4 +37,3 @@ int main() {
     free(input);
     return 0;
 }
-

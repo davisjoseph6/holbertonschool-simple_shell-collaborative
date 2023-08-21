b5e00eff18f27c41cb66f8bdb1c66c409ee5b093
Davis <davisjoseph767@gmail.com>
2023-08-21 01:11:47 -0700

    updated shell.c

--- shell.c
+++ shell.c
@@ -9,62 +9,27 @@
 
 void execute_command(char *command)
 {
-	pid_t pid = fork();
-	
-	if (pid == -1)
-	{
-		perror("fork");
-	}
-	else if (pid == 0)
-	{
-		/* Child process */
-		char *args[2];
-		args[0] = command;
-		args[1] = NULL;
-		execve(command, args, NULL);
-		perror("./shell");
-		_exit(1);
-	}
-	else
-	{
-		/* Parent process */
-		wait(NULL);
-	}
-}
+    pid_t pid = fork();
 
-int main()
-{
-        char *input = NULL;
-        size_t len = 0;
-        ssize_t read_len;
-
-        while (1)
-        {
-                printf("#cisfun$ ");
-                read_len = getline(&input, &len, stdin);
-
-                if (read_len == -1)
-                {
-                        if (feof(stdin))
-                        {
-                                printf("\n");
-                                break;
-                        }
-                        else
-                        {
-                                perror("getline");
-                                exit(1);
-                        }
-                }
-
-                if (input[read_len - 1] == '\n')
-                {
-                        input[read_len - 1] = '\0'; /* Remove the newline character */
-                }
-
-                execute_command(input);
-        }
-
-    free(input);
-    return 0;
+    if (pid == -1)
+    {
+        perror("fork");
+    }
+    else if (pid == 0)
+    {
+        /* Child process */
+        char *args[4];
+        args[0] = "sh";
+        args[1] = "-c";
+        args[2] = command;
+        args[3] = NULL;
+        execvp("sh", args);
+        perror("./shell");
+        _exit(1);
+    }
+    else
+    {
+        /* Parent process */
+        wait(NULL);
+    }
 }

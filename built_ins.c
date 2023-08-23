525d02f705c0c71fd7e4ac0e02d2519c54fcb28c
abdel <holbertonc21@gmail.com>
2023-08-23 06:23:56 -0700

    fixed functions

--- built_ins.c
+++ built_ins.c
@@ -11,13 +11,13 @@
 void builtins(char *line, char **args, char **env, int *ex_st)
 {
 
-	if (_strcmp(args[0], "exit") == 0)
+	if (strcmp(args[0], "exit") == 0)
 	{
 		free(args);
 		free(line);
 		exit(*ex_st);
 	}
 
-	if (_strcmp(args[0], "env") == 0)
+	if (strcmp(args[0], "env") == 0)
 		printenv(env, ex_st);
 }
